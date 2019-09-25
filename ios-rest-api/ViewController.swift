9be1d9fb7e835a5adae6e946f26690094b21085a
Brian Bansenauer <brianb@cascadia.edu>
2019-09-25 15:28:28 -0700

    added TODO

--- ios-rest-api/ViewController.swift
+++ ios-rest-api/ViewController.swift
@@ -8,11 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+    let DomainURL = "http://216.186.69.45/services/device/"
+    
+    class User {
+        
+        static func fetch(){
+            let URLstring = DomainURL + "users/"
+            if let url = URL.init(string: URLstring){
+                let task = URLSession.shared.dataTask(with: url, completionHandler:
+                    //TODO: Add closure
+                )
+                task.resume()
+            }
+        }
+    }
 
+class ViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        User.fetch()
     }
 
 
